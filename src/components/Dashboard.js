fbfe0cc80c46c7e3072fd9cdc8edf4de216500f1
yuhaoyann <yuhaoyann@gmail.com>
2021-12-29 00:06:53 -0500

    set up loading page

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,9 +2,18 @@ import React, { Component } from "react";
 
 import classnames from "classnames";
 
+import Loading from "./Loading";
+
 class Dashboard extends Component {
+  state = {
+    loading: false
+  };
   render() {
     const dashboardClasses = classnames("dashboard");
+
+    if (this.state.loading) {
+      return <Loading />;
+    }
 
     return <main className={dashboardClasses} />;
   }
